c75b66666312392f7cb49f8da0960c61e2f3118d
Sadeeptha Palihawadana <psadeeptha417@gmail.com>
2026-01-30 07:29:36 +0530

    Create tabs UI using javascript tab structure

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,47 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+const TabsLayout = () => {
+  return (
+    <Tabs screenOptions={{
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'green',
+        tabBarStyle:{
+            backgroundColor: '#1e293b',
+            borderTopWidth: 1,
+            elevation: 5,
+            borderTopColor: 'yellow',
+            height: 90,
+            paddingBottom: 30,
+            paddingTop: 10,
+        },
+        tabBarLabelStyle:{
+            fontSize: 14,
+            fontWeight: '600',
+        },
+        headerShown: false,
+    }}    >
+        <Tabs.Screen
+            name='index'
+            options={{
+                title: 'Todos',
+                tabBarIcon:({color,size}) => (
+                    <Ionicons name='flash-outline' size={size} color={color}/>
+                )
+            }}
+        />
+        <Tabs.Screen
+            name='settings'
+            options={{
+                title: 'Settings',
+                tabBarIcon:({color,size}) => (
+                    <Ionicons name='settings' size={size} color={color}/>
+                )
+            }}
+        />
+    </Tabs>
+  );
+};
+
+export default TabsLayout
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style = {styles.container}>
+      <Text style = {styles.content}>Edit app/index.tsx to edit this screen.123</Text>
+      <Text>Hi</Text>
+      
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  content:{
+    fontSize: 22,
+    rowGap: 10,
+
+
+  }
+});
